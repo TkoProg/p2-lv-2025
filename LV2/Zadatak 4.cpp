98be8c7c5fb0ab599126b83cccc5ec93962d5577
tamiroladejo <koltam09@gmail.com>
2025-03-22 14:01:31 +0100

    LV 2 Zadatak 4 commit

---
+++ LV2/Zadatak 4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, place, brojac(0), najveca(0), holding(0), broj(0);
+    while (cin>>n, n!= 0) {
+        place = n;
+        while (n != 0) {
+            if (n % 10 > najveca) najveca = n % 10;
+            n /= 10;
+        }
+        n = place;
+        while (n != 0) {
+            if (n % 10 == najveca) brojac++;
+            n /= 10;
+        }
+        if (brojac > holding) {
+            broj = place;
+            holding = brojac;
+        }
+    }
+    cout<<"Trazeni broj je: "<<broj;
+}
